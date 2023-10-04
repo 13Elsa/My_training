a049d288944d679235ccaa14ede0f8d9b938e6fb
13Elsa <125810161+13Elsa@users.noreply.github.com>
2023-10-04 19:07:24 +0530

    string_rotation.py

--- string_rotation.py
+++ string_rotation.py
@@ -1,37 +1,9 @@
-def reverse_words(sentence):
-  """Reverses the words in a sentence.
-
-  Args:
-    sentence: A string.
-
-  Returns:
-    A string with the words reversed and the punctuation marks at the end of
-    the words.
-  """
-
-  words = sentence.split()
-  reversed_words = []
-  for word in words:
-    reversed_word = ""
-    for i in reversed(range(len(word))):
-      reversed_word += word[i]
-    reversed_words.append(reversed_word)
-
-  # Add the punctuation marks at the end of the words.
-  for i in range(len(reversed_words)):
-    reversed_words[i] += sentence[len("".join(reversed_words[:i]))]
-
-  reversed_sentence = " ".join(reversed_words)
-  return reversed_sentence
-
-
-def main():
-  """Takes input and gives output as mentioned in the question."""
-
-  sentence = input("Enter a sentence: ")
-  reversed_sentence = reverse_words(sentence)
-  print(f"The reversed sentence is: {reversed_sentence}")
-
-
-if __name__ == "__main__":
-  main()
+s=input("Enter String:").split()
+key=int(input("Enter key value: "))
+result=" "
+for i in range(len(s)):
+    result=result+s[i][key:len(s[i]):1]
+    if i+1!=len(s):
+        result=result+s[i+1][:key]+" "
+result=result+s[0][:key]
+print(result)
