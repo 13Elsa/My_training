d407dd1e3da8a43fb57599bc9a8c8957383d43a7
13Elsa <125810161+13Elsa@users.noreply.github.com>
2023-10-04 21:31:16 +0530

    Create magic_square_recursion.py
    Magic square code using recursion in python.

---
+++ magic_square_recursion.py
@@ -0,0 +1,22 @@
+def ms(n):
+    sq=[[0]*n for i in range(n)]
+    def fill(i,j,num):
+        if num>(n*n):
+            return sq
+        if i<0 and j==n:
+            i=0
+            j=n-2
+        else:
+            if i<0:                       # if row is out of bound 
+                i=n-1
+            if j==n:                      # if column is out of bound
+                j=0
+        if sq[i][j]:
+            i=i+1
+            j=j-2
+            return fill(i,j,num)
+        sq[i][j]=num
+        return fill(i-1,j+1,num+1)
+    return fill(n//2,n-1,1)
+n=int(input())
+print(ms(n))
